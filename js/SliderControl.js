17be5bd2f44a3d8bec21c6ead5b6976fcb728de4
Alejandro <aaschwedhelm@gmail.com>
2014-12-15 11:25:05 -0500

    fixed changes

--- js/SliderControl.js
+++ js/SliderControl.js
@@ -6,6 +6,7 @@ L.Control.SliderControl = L.Control.extend({
         minValue: -1,
         markers: null,
         range: false,
+        activeLayers: [],
         follow: false
     },
 
@@ -36,7 +37,7 @@ L.Control.SliderControl = L.Control.extend({
 
         // Create a control sliderContainer with a jquery ui slider
         var sliderContainer = L.DomUtil.create('div', 'slider', this._container);
-        $(sliderContainer).append('<div id="leaflet-slider" style="width:200px"><div class="ui-slider-handle"></div><div id="slider-timestamp" style="width:200px; margin-top:10px;background-color:#FFFFFF"></div></div>');
+        $(sliderContainer).append('<div id="leaflet-slider" style="width:200px"><div class="ui-slider-handle"></div><div id="slider-timestamp" style="width:200px; margin-top:10px;"></div></div>');
         //Prevent map panning/zooming while using the slider
         $(sliderContainer).mousedown(function () {
             map.dragging.disable();
@@ -75,67 +76,48 @@ L.Control.SliderControl = L.Control.extend({
     },
 
     startSlider: function () {
+        var breakPoints = [25, 1750, 8618, 18162, 27031, 27786, 29803];
         _options = this.options;
         $("#leaflet-slider").slider({
             range: _options.range,
             value: _options.minValue + 1,
             min: _options.minValue,
-            max: _options.maxValue +1,
+            max: breakPoints[6],
             step: 1,
             slide: function (e, ui) {
                 var map = _options.map;
-                if(!!_options.markers[ui.value]) {
-                    // If there is no time property, this line has to be removed (or exchanged with a different property)
-                    if(_options.markers[ui.value].feature !== undefined) {
-                        if(_options.markers[ui.value].feature.properties.time){
-                            if(_options.markers[ui.value]) $('#slider-timestamp').html(_options.markers[ui.value].feature.properties.time.substr(0, 19));
-                        }else {
-                            console.error("You have to have a time property");
+
+                for (var i = 0; i < breakPoints.length; i++) {
+                    if (ui.value >= breakPoints[i]) {
+                        var layerIsActive = false;
+                        for (var j = 0; j < _options.activeLayers.length; j++) {
+                            if (breakPoints[i] === _options.activeLayers[j]) {
+                                layerIsActive = true;
+                            }
                         }
-                    }else {
-                        // set by leaflet Vector Layers
-                        if(_options.markers [ui.value].options.time){
-                            if(_options.markers[ui.value]) $('#slider-timestamp').html(_options.markers[ui.value].options.time.substr(0, 19));
-                        }else {
-                            console.error("You have to have a time property");
+                        if (!layerIsActive && breakPoints[i] !== breakPoints[6]) {
+                           _options.map.addLayer(_options.markers[breakPoints[i]]);
+                           _options.activeLayers.push(breakPoints[i]);
                         }
-                    }
-                    
-                    var i;
-                    if(_options.range){
-                        // jquery ui using range
-                        for (i = ui.values[0]; i <= ui.values[1]; i++){
-                           if(_options.markers[i]) map.addLayer(_options.markers[i]);
+                        
+                        if (breakPoints[i] !== breakPoints[6]) {
+                            $('#slider-timestamp').html(_options.markers[breakPoints[i]].options.time.substr(0, 19));
                         }
-                        for (i = _options.maxValue; i > ui.values[1]; i--) {
-                            if(_options.markers[i]) map.removeLayer(_options.markers[i]);
-                        }
-                        for (i = _options.minValue; i < ui.values[0]; i++) {
-                            if(_options.markers[i]) map.removeLayer(_options.markers[i]);
-                        }
-                    }else if(_options.follow){
-                        for (i = _options.minValue; i < (ui.value - _options.follow); i++) {
-                            if(_options.markers[i]) map.removeLayer(_options.markers[i]);
-                        }
-                        for (i = (ui.value - _options.follow); i < ui.value ; i++) {
-                            if(_options.markers[i]) map.addLayer(_options.markers[i]);
-                        }
-                        for (i = ui.value; i <= _options.maxValue; i++) {
-                            if(_options.markers[i]) map.removeLayer(_options.markers[i]);
-                        }
-                    }else{
-                        // jquery ui for point before
-                        for (i = _options.minValue; i <= ui.value ; i++) {
-                            if(_options.markers[i]) map.addLayer(_options.markers[i]);
-                        }
-                        for (i = (ui.value + 1); i <= _options.maxValue; i++) {
-                            if(_options.markers[i]) map.removeLayer(_options.markers[i]);
+                    } else {
+                        if (ui.value < breakPoints[i] && i !== 6) {
+                            _options.map.removeLayer(_options.markers[breakPoints[i]]);
+                            for (var k = 0; k < _options.activeLayers.length; k++) {
+                                if (breakPoints[i] === _options.activeLayers[k]) {
+                                    _options.activeLayers.splice(k, 1);
+                                }
+                            }
                         }
                     }
                 }
             }
         });
         _options.map.addLayer(_options.markers[_options.minValue]);
+        _options.activeLayers.push(_options.minValue);
     }
 });
 
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var sliderControl = null
 
+var myGeoJSONLayers = [];
+
 var map = L.map('map').setView([11.19059, -74.20099], 13);
 var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
@@ -8,97 +10,80 @@ var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/
         //All polygons within 1 Layer: [geojsonLayer]
     }).addTo(map);
 
- //Fetch all the GeoJSON files
-$.getJSON('./js/1989.geojson', function(data) {
-    window.data = data;
-    var geojsonLayer = L.geoJson(data.features, {
-        style: {
-            fillColor: "rgba(255,0,0,.6)",
-            fillOpacity: ".6",
-            color: "rgba(255,0,0,0)",
-            weight: "0",
-            opacity: ".6",
-        }
-        },
-        {time: "2013-01-22 08:42:26+01"
-    })
-});
 
-$.getJSON('./js/2000.geojson', function(data) {
-    window.data = data;
-    var geojsonLayer2 = L.geoJson(data.features, {
-        style: {
-            fillColor: "rgba(255,0,0,.6)",
-            fillOpacity: ".6",
-            color: "rgba(255,0,0,0)",
-            weight: "0",
-            opacity: ".6",
-        }
-        },
-        {time: "2013-01-22 10:00:26+01"
-    })
-});
+// var populationLegend = L.control({position: 'bottomright'});
+// var populationChangeLegend = L.control({position: 'bottomright'});
 
-$.getJSON('./js/2013.geojson', function(data) {
-    window.data = data;
-    var geojsonLayer3 = L.geoJson(data.features, {
-        style: {
-            fillColor: "rgba(255,0,0,.6)",
-            fillOpacity: ".6",
-            color: "rgba(255,0,0,0)",
-            weight: "0",
-            opacity: ".6",
-        }
-        },
-        {time: "2013-01-22 10:03:29+01"
-    })
-});
+// populationLegend.onAdd = function (map) {
+// var div = L.DomUtil.create('div', 'info legend');
+//     div.innerHTML +=
+//     '<img src="legend.png" alt="legend" width="400" height="35">';
+// return div;
+// };
 
-$.getJSON('./js/2026.geojson', function(data) {
-    window.data = data;
-    var geojsonLayer4 = L.geoJson(data.features, {
-        style: {
-            fillColor: "rgba(255,0,0,.6)",
-            fillOpacity: ".6",
-            color: "rgba(255,0,0,0)",
-            weight: "0",
-            opacity: ".6",
-        }
-        })
-});
+// populationChangeLegend.onAdd = function (map) {
+// var div = L.DomUtil.create('div', 'info legend');
+//     div.innerHTML +=
+//     '<img src="change_legend.png" alt="legend" width="400" height="35">';
+// return div;
+// };
 
-$.getJSON('./js/2039.geojson', function(data) {
-    window.data = data;
-    var geojsonLayer5 = L.geoJson(data.features, {
-        style: {
-            fillColor: "rgba(255,0,0,.6)",
-            fillOpacity: ".6",
-            color: "rgba(255,0,0,0)",
-            weight: "0",
-            opacity: ".6",
-        }
-        })
-});
+// // Add this one (only) for now, as the Population layer is on by default
+// populationLegend.addTo(map);
 
-$.getJSON('./js/2078.geojson', function(data) {
-    window.data = data;
-    var geojsonLayer6 = L.geoJson(data.features, {
-        style: {
-            fillColor: "rgba(255,0,0,.6)",
-            fillOpacity: ".6",
-            color: "rgba(255,0,0,0)",
-            weight: "0",
-            opacity: ".6",
-        }
-        })
-});
+// map.on('overlayadd', function (eventLayer) {
+//     // Switch to the Population legend...
+//     if (eventLayer.name === 'Population') {
+//         this.removeControl(populationChangeLegend);
+//         populationLegend.addTo(this);
+//     } else { // Or switch to the Population Change legend...
+//         this.removeControl(populationLegend);
+//         populationChangeLegend.addTo(this);
+//     }
+// });
 
-layerGroup = L.layerGroup(['geojsonLayer', 'geojsonLayer2', 'geojsonLayer3', 'geojsonLayer4','geojsonLayer5', 'geojsonLayer6' ]);
-var sliderControl = L.control.sliderControl({position: "topright", layer:layerGroup, range: true});
-        //Make sure to add the slider to the map
-        map.addControl(sliderControl);
-        //And initialize the slider
-        sliderControl.startSlider();
+var numberOfJSONFiles = 6;
+
+function getJSON (url, rgb, time) {
+    $.getJSON(url, function(data) {
+        // window.data = data;
+        var geojsonLayer = L.geoJson(data.features, 
+        {
+            style: {
+                fillColor: rgb,
+                fillOpacity: ".6",
+                color: "rgba(0,255,0,0.5)",
+                weight: "0",
+                opacity: ".6"
+            },
+            time: time
+        });
+        myGeoJSONLayers.push(geojsonLayer);
+        receivedData();
+    });
+}
+
+// Fetch all the GeoJSON files
+getJSON('./js/1989.geojson', 'rgba(255,0,0,.6)', 'Year: 1989');
+getJSON('./js/2000.geojson', 'rgba(255,128,0,.6)', 'Year: 2000');
+getJSON('./js/2013.geojson', 'rgba(255,255,0,.6)', 'Year: 2013');
+getJSON('./js/2026.geojson', 'rgba(128,255,0,.6)', 'Year: 2026');
+getJSON('./js/2039.geojson', 'rgba(0,255,255,.6)', 'Year: 2039');
+getJSON('./js/2078.geojson', 'rgba(0,0,255,.6)', 'Year: 2078');
+
+
+function receivedData () {
+    if (myGeoJSONLayers.length !== numberOfJSONFiles) {
+        return;
+    }
+    var layerGroup = L.layerGroup(myGeoJSONLayers);
+    var sliderControl = L.control.sliderControl({position: "topright", layer:layerGroup});
+    //Make sure to add the slider to the map
+    map.addControl(sliderControl);
+    //And initialize the slider
+    sliderControl.startSlider();
+}
+
 
 
 
